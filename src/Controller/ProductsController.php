85a56f69de98e7f4e35c0e1389173ed67aea2b59
MohamedNourTN <nourmohamed603@gmail.com>
2023-02-22 19:56:05 +0100

    Template

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Products;
+use App\Form\ProductsType;
+use App\Repository\ProductsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+
+#[Route('/products')]
+class ProductsController extends AbstractController
+{
+    #[Route('/', name: 'app_products_index', methods: ['GET'])]
+    public function index(ProductsRepository $productsRepository): Response
+    {
+        return $this->render('products/index.html.twig', [
+            'products' => $productsRepository->findAll(),
+        ]);
+    }
+    #[Route('/view', name: 'app_products_View', methods: ['GET'])]
+    public function View(ProductsRepository $ProductsRepository): Response
+    {
+        return $this->render('products/view.html.twig', [
+            'products' => $ProductsRepository->findAll(),
+        ]);
+    }
+    #[Route('/new', name: 'app_products_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ProductsRepository $productsRepository, SluggerInterface $slugger): Response
+    {
+        $product = new Products();
+        $form = $this->createForm(ProductsType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productsRepository->save($product, true);
+            $ProductPictureFile = $form->get('ProductPicture')->getData();
+            if ($ProductPictureFile) {
+                $originalFilename  = pathinfo($ProductPictureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$ProductPictureFile->guessExtension();
+
+                try {
+                    $ProductPictureFile->move(
+                        $this->getParameter('Products_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                }
+
+                $product->setProductPicture($newFilename);
+            }
+            return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('products/new.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_products_show', methods: ['GET'])]
+    public function show(Products $product): Response
+    {
+        return $this->render('products/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_products_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Products $product, ProductsRepository $productsRepository): Response
+    {
+        $form = $this->createForm(ProductsType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productsRepository->save($product, true);
+
+            return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('products/edit.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_products_delete', methods: ['POST'])]
+    public function delete(Request $request, Products $product, ProductsRepository $productsRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+            $productsRepository->remove($product, true);
+        }
+
+        return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Form/ProductsCategoryType.php
+++ src/Form/ProductsCategoryType.php
@@ -2,27 +2,25 @@
 
 namespace App\Form;
 
-use App\Entity\Produit;
+use App\Entity\ProductsCategory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProduitType extends AbstractType
+class ProductsCategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomproduit')
-            ->add('prixproduit')
-            ->add('dateproduit')
-            ->add('type')
+            ->add('Categoryname')
+            ->add('Type')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Produit::class,
+            'data_class' => ProductsCategory::class,
         ]);
     }
 }
